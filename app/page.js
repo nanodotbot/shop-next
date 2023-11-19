f462b7a0951fe9b16b93685a50d4d497fa9c22a4
nanodotbot <nanodotbot@gmail.com>
2023-11-19 13:13:58 +0100

    got rid of the header

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default async function Home() {
 
     return (
         <div>
-            <Header />
+            {/* <Header /> */}
             <main className={styles.main}>
                 <Items items={items} />
                 <CategoryDialog />
